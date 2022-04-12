27469e65990dd51dabfdb96bb6f643f8c7bd6d66
unknown <rellowy@gmail.com>
2022-04-12 21:04:30 +0300

    Added models

---
+++ backend/models/errorModel.js
@@ -0,0 +1,5 @@
+export default class Error {
+  constructor(errorMessage) {
+    this.message = errorMessage;
+  }
+}
---
+++ backend/models/registrationCredentialsModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['SHIPPER', 'DRIVER'],
+      required: true,
+    },
+  },
+  {
+    timestamps: { createdAt: 'created_date', updatedAt: false },
+  }
+);
+
+module.exports = mongoose.model('registrationCredentials', schema);
