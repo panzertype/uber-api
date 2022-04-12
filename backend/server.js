fac368becbf6212954eb5dced36c40e328c8e819
unknown <rellowy@gmail.com>
2022-04-12 17:57:12 +0300

    Created basic server, added environment variables.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('Hello World')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port);
