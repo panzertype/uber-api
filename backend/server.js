170d199824488ee42f9f5b39c99f4f3224ffd839
unknown <rellowy@gmail.com>
2022-04-20 12:54:23 +0300

    Added morgan package

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const port = process.env.PORT || 8080;
@@ -14,6 +15,7 @@ const trucksRouter = require('./routes/trucksRoutes');
 const usersRouter = require('./routes/usersRoutes');
 
 app.use(cors());
+app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/api/auth', authRouter);
